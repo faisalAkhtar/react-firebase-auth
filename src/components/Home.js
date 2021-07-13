268ae88ef27be4ff3fdfca8a126081fece05ef38
Faisal Vostro - Ubuntu <faisalakhtar1999@gmail.com>
2021-07-14 00:29:10 +0530

    firebase authentication

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
+import { auth } from "../config/firebase"
+
 export default function Home() {
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
+	const handleSignOut = () => {
+		auth.signOut()
+			.then(() => {
+				console.log("Sign-out successful")
+			})
+			.catch(err => {
+				alert(err.message)
+				console.log(err)
+			})
+	}
+
+	return (
+		<>
+			<h1>Home</h1>
+			<button onClick={handleSignOut}>Sign out</button>
+		</>
+	)
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,44 @@
-export default function Login() {
-    return (
-        <>
-            <h1>Login</h1>
-        </>
-    )
+import { useCallback, useContext } from "react"
+import { Redirect, withRouter } from "react-router";
+import { auth } from "../config/firebase";
+import { AuthContext } from "../utils/Auth";
+
+function Login({ history }) {
+	const handleLogin = useCallback(async ev => {
+		ev.preventDefault()
+		const { mail, pass } = ev.target.elements;
+		await auth.signInWithEmailAndPassword(mail.value, pass.value)
+			.then(() => {
+				console.log("Sign-in successful")
+				history.push("/")
+			})
+			.catch(err => {
+				alert(err.message)
+				console.log(err)
+			})
+	}, [history])
+
+	const { currentUser } = useContext(AuthContext)
+	if (currentUser) return <Redirect to="/" />
+
+	return (
+		<>
+			<h1>Login</h1>
+			<form onSubmit={handleLogin}>
+				<label>
+					Email
+					<input name="mail" type="email" required autoFocus />
+				</label>
+
+				<label>
+					Password
+					<input name="pass" type="password" required />
+				</label>
+
+				<button type="submit">Submit</button>
+			</form>
+		</>
+	)
 }
+
+export default withRouter(Login)

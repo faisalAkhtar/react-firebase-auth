0f111685cdc6260ca79c003b37a37303300a0bb8
Faisal Vostro - Ubuntu <faisalakhtar1999@gmail.com>
2021-07-13 23:14:06 +0530

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-function App() {
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Home from "./components/Home"
+import Login from "./components/Login"
+
+const Error404 = () => {
 	return (
 		<>
-			<h1>Hello World!</h1>
+			<h3>Error 404</h3>
+			<h1>Page not found</h1>
 		</>
+	)
+}
+
+function App() {
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Home} />
+				<Route exact path="/login" component={Login} />
+				<Route component={Error404} />
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
